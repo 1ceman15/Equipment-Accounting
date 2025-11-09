af4c241d0210a75f9ad3ea7c9487f93fdd498499
Muradly Muhammed <mmuradly2@gmail.com>
2025-11-09 16:09:22 +0300

    first commit

---
+++ Equipment Accounting Backend/src/main/kotlin/org/iceman/equipment_accounting/EquipmentAccountingBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.iceman.equipment_accounting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EquipmentAccountingBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<EquipmentAccountingBackendApplication>(*args)
+}
---
+++ Equipment Accounting Backend/src/test/kotlin/org/iceman/equipment_accounting/EquipmentAccountingBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.iceman.equipment_accounting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EquipmentAccountingBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
